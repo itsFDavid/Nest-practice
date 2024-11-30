27aa4436606a7da6b36e77c48bd0b6ca4f4884b3
itsFDavid <FDavid04@icloud.com>
2024-11-30 17:18:04 -0600

    feat(product): enhance CreateProductDto with validation for title, price, description, stock, slug, sizes, and gender

--- 04-teslo-shop/src/products/dto/create-product.dto.ts
+++ 04-teslo-shop/src/products/dto/create-product.dto.ts
@@ -1,4 +1,34 @@
+import { IsArray, IsIn, IsInt, IsNumber, IsOptional, IsPositive, IsString, MinLength } from "class-validator";
+
 export class CreateProductDto {
 
+    @IsString()
+    @MinLength(1)
+    title: string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?: number;
     
+    @IsString()
+    @MinLength(1)
+    @IsOptional()
+    description?: string;
+
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    stock?: number;
+
+    @IsString()
+    @IsOptional()
+    slug?: string; 
+    
+    @IsString({ each: true })
+    @IsArray()
+    sizes: string[];
+
+    @IsIn(["men", "women", "kids", "unisex"])
+    gender: string;
 }
