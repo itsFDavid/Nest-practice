e8185e5bf9fbc71b8dda1ea0fd6a6979c221c15c
itsFDavid <FDavid04@icloud.com>
2024-12-25 21:07:38 -0600

    docs(products): add comment to clarify PATCH request handling in ProductsController

--- 04-teslo-shop/src/products/products.controller.ts
+++ 04-teslo-shop/src/products/products.controller.ts
@@ -27,6 +27,7 @@ export class ProductsController {
     return this.productsService.findOne(id);
   }
 
+  // this controller method will be called when a PATCH request is made to the /products/:id endpoint
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
