477df1cdf4258f230d623881dac30aeb7818afb1
itsFDavid <FDavid04@icloud.com>
2025-01-23 15:10:45 -0600

    feat(deps): upgrade NestJS packages to version 11 and add Swagger support

--- 04-teslo-shop/src/main.ts
+++ 04-teslo-shop/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
@@ -11,6 +13,18 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     })
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Teslo Restful API')
+    .setDescription('The Teslo API is a RESTful API that allows you to interact with the Teslo application.')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('/api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+  logger.log(`Server running on http://localhost:${process.env.PORT ?? 3000}`, 'Bootstrap');
 }
 bootstrap();
