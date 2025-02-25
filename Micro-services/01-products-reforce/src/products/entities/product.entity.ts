1d7f6b919a7ccf47dda7b6dae5a6c35886dcbf7b
itsFDavid <FDavid04@icloud.com>
2025-02-24 18:32:14 -0600

    feat(products): enhance product entity and DTO with validation, add product creation logic

--- Micro-services/01-products-reforce/src/products/entities/product.entity.ts
+++ Micro-services/01-products-reforce/src/products/entities/product.entity.ts
@@ -1,1 +1,12 @@
-export class Product {}
+export class Product {
+
+  constructor(
+    public id: string,
+    public name: string,
+    public description: string,
+    public price: number
+  ){}
+
+
+  // TODO: updateWith
+}
