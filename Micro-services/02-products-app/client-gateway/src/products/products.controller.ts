b803d2bf1ad2422d30572ba582273e7eecc58364
itsFDavid <FDavid04@icloud.com>
2025-02-26 21:40:11 -0600

    feat(products): enhance product retrieval with error handling and update findOne method

--- Micro-services/02-products-app/client-gateway/src/products/products.controller.ts
+++ Micro-services/02-products-app/client-gateway/src/products/products.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Delete, Get, Inject, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Inject, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
 import { PRODUCT_SERVICE } from 'src/config';
 
@@ -21,8 +22,16 @@ export class ProductsController {
   }
   
   @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number){
-    return `This action returns a #${id} product`;
+  async findOne(@Param('id', ParseIntPipe) id: number){
+    try{
+      const product = await firstValueFrom(
+        this.productsClient
+          .send({ cmd: 'find_one_product' }, { id })
+      );
+      return product;
+    }catch(error){
+      throw new BadRequestException(error);
+    }
   }
 
   @Delete(':id')
--- Micro-services/02-products-app/products-ms/src/products/products.service.ts
+++ Micro-services/02-products-app/products-ms/src/products/products.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit{
@@ -47,11 +48,11 @@ export class ProductsService extends PrismaClient implements OnModuleInit{
   }
 
   async findOne(id: number) {
-    const product = await this.product.findUnique({
+    const product = await this.product.findFirst({
       where: { id, available: true }
     });
     if(!product) {
-      throw new NotFoundException(`Product with id ${id} not found`);
+      throw new RpcException(`Product with id ${id} not found`);
     }
     return product;
 
