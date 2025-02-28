c1b6332e5988412b22e9b39417a30f26e561e668
itsFDavid <FDavid04@icloud.com>
2025-02-28 16:30:41 -0600

    feat(products): add validateProducts method to ProductsService and corresponding controller endpoint

--- Micro-services/02-products-app/products-ms/src/products/products.service.ts
+++ Micro-services/02-products-app/products-ms/src/products/products.service.ts
@@ -88,4 +88,26 @@ export class ProductsService extends PrismaClient implements OnModuleInit{
       message: `Product with id ${id} deleted successfully`
     }
   }
+
+  async validateProducts(ids: number[]) {
+
+    ids = Array.from(new Set(ids));
+    
+    const products = await this.product.findMany({
+      where: { 
+        id: {
+          in: ids
+        }
+      }
+    });
+
+    if(products.length !== ids.length) {
+      throw new RpcException({
+        message: 'Invalid product ids',
+        status: HttpStatus.BAD_REQUEST
+      });
+    }
+
+    return products;
+  }
 }
