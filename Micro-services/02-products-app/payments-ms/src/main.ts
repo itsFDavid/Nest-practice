523cf95af3a882909cfff30ea4c1188db87d9f6c
itsFDavid <FDavid04@icloud.com>
2025-03-04 17:20:07 -0600

    add validation and DTO for payment session; update controller and service to handle payment session data

--- Micro-services/02-products-app/payments-ms/src/main.ts
+++ Micro-services/02-products-app/payments-ms/src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger('Payments-ms');
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      forbidNonWhitelisted: true
+    })
+  );
   logger.log(`Payments-ms is running on: ${envs.port}`);
   await app.listen(envs.port);
 }
--- Micro-services/02-products-app/payments-ms/src/payments/payments.service.ts
+++ Micro-services/02-products-app/payments-ms/src/payments/payments.service.ts
@@ -1,29 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { envs } from 'src/config/envs';
 import Stripe from 'stripe';
+import { PaymentSessionDto } from './dto/payment-session.dto';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
 
-  async createPaymentSession() {
+  async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+    const { currency, items} = paymentSessionDto;
+    
+    const lineItems = items.map( items => {
+      return {
+        price_data: {
+          currency: currency,
+          product_data: {
+            name: items.name,
+          },
+          unit_amount: Math.round(items.price * 100), // <- Esto seria 20.00
+        },
+        quantity: items.quantity,
+      }
+    })
+
     const session = this.stripe.checkout.sessions.create({
       // Colocar el id de la orden
       payment_intent_data: {
         metadata: {}
       },
-      line_items: [
-        {
-          price_data: {
-            currency: 'usd',
-            product_data: {
-              name: 'T-shirt',
-            },
-            unit_amount: 2000, // <- Esto seria 20.00
-          },
-          quantity: 2,
-        }
-      ],
+      line_items: lineItems,
       mode: 'payment',
       success_url: 'http://localhost:3000/payments/success',
       cancel_url: 'http://localhost:3000/payments/cancel',
